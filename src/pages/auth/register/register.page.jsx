b2da1692b274c41315c5e8b9ecf7dc4c37bc535a
Jupiter82 <jupiterbade@gmail.com>
2024-05-14 09:08:58 +0545

    2024-5-14 work on register

--- src/pages/auth/register/register.page.jsx
+++ src/pages/auth/register/register.page.jsx
@@ -1,5 +1,11 @@
-import { Container, Col, Row, Form, Button } from "react-bootstrap"
+import { Container, Col, Row, Form, Button, FormControl } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import Select from 'react-select'
+
+const options = [
+    { value: 'customer', label: 'Customer' },
+    { value: 'seller', label: 'Seller' }
+  ]
 
 const RegisterPage = () =>{
     const submitForm = (e) =>{
@@ -19,7 +25,7 @@ const RegisterPage = () =>{
                     <Col className={`bg-dark-subtle py-3`} md={{offset:2, span:8}} lg={{offset:3, span:6}}>
                         <Form onSubmit={submitForm}>
                             <Form.Group className="row mb-3">
-                                <Form.Label className="col-dm-3 text-dark">Full Name:</Form.Label>
+                                <Form.Label className="col-sm-3 text-dark">Full Name:</Form.Label>
                                 <Col sm={9}>
                                     <Form.Control
                                     required
@@ -29,7 +35,7 @@ const RegisterPage = () =>{
                                 </Col>
                             </Form.Group>
                             <Form.Group className="row mb-3">
-                                <Form.Label className="col-dm-3 text-dark">Email(username):</Form.Label>
+                                <Form.Label className="col-sm-3 text-dark">Email(username):</Form.Label>
                                 <Col sm={9}>
                                     <Form.Control
                                     type="password"
@@ -37,6 +43,37 @@ const RegisterPage = () =>{
                                     placeholder="Enter your Password"
                                     size="sm">
                                     </Form.Control>
+                                </Col>
+                            </Form.Group>
+                            <Form.Group className="row mb-3">
+                                <Form.Label className="col-sm-3 text-dark">Address:</Form.Label>
+                                <Col sm={9}>
+                                    <Form.Control
+                                    as={"textarea"}
+                                    required
+                                    rows={5}
+                                    style={{resize:"none"}}
+                                    placeholder="Enter your Password"
+                                    size="sm">
+                                    </Form.Control>
+                                </Col>
+                            </Form.Group>
+                            <Form.Group className="row mb-3">
+                                <Form.Label className="col-sm-3 text-dark">Role:</Form.Label>
+                                <Col sm={9}>
+                                <Select options={options} isClearable className="form-select-sm"/>
+                                    {/* <Form.Select size="sm" name="role" required>
+                                        <option value=''>--Select Any One---</option>
+                                        <option value='customer'>Customer</option>
+                                        <option value='seller'>Seller</option>
+                                    </Form.Select> */}
+                                </Col>
+                            </Form.Group>
+                            <Form.Group className="row mb-3">
+                                <Form.Label className="col-sm-3 text-dark">Images:</Form.Label>
+                                <Col sm={7}>
+                                        <Form.Control></Form.Control>
+
                                 </Col>
                             </Form.Group>
                             <Form.Group className="row mb-3">
